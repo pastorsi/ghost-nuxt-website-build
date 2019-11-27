e24d2e83fa3969c6caec330e554f58a5ffef9a22
Simon Measures <pastorsi@gmail.com>
2019-11-27 07:45:35 +0000

    Added columns to my Home Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <section class="section">
     <div class="content">
-      <h1 class="title">{{ page.title }}</h1>
-      <div v-html="page.html">{{ page.html }}</div>
+      <div class="columns">
+        <div class="column"></div>
+        <div class="column is-two-thirds">
+          <h1 class="title has-text-info">{{ page.title }}</h1>
+          <div v-html="page.html">{{ page.html }}</div>
+        </div>
+        <div class="column"></div>
+      </div>
     </div>
   </section>
 </template>
