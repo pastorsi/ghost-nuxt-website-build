5a5f9fba93b63e530b871ea5593981625438572b
Simon Measures <pastorsi@gmail.com>
2019-11-29 22:55:37 +0000

    Better organised template for affiliated churches page

---
+++ layouts/AltDesign.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Logo></Logo>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style></style>
--- pages/affiliated-churches.vue
+++ pages/affiliated-churches.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        Affiliated Churches and Ministries
-      </h1>
-      <h2 class="subtitle">
-        These are mostly in the Cheltenham area
-      </h2>
-      <div v-for="(post, index) in posts" :key="index" class="card">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">{{ post.title }}</p>
-              <p class="subtitle is-6">Author: {{ post.authors[0].name }}</p>
+  <section class="section">
+    <div class="columns">
+      <div class="column"></div>
+      <div class="column is-two-thirds">
+        <h1 class="title has-text-info">
+          Affiliated Churches and Ministries
+        </h1>
+        <div class="container">
+          <div v-for="(post, index) in posts" :key="index" class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{{ post.title }}</p>
+                  <p class="subtitle is-6">
+                    Author: {{ post.authors[0].name }}
+                  </p>
+                </div>
+              </div>
+              <div class="content">
+                {{ post.excerpt }}
+                <br />
+                <time>Date posted: {{ post.updated_at }}</time>
+                <br />
+                <nuxt-link :to="{ path: post.slug }">View this post!</nuxt-link>
+              </div>
             </div>
-          </div>
-          <div class="content">
-            {{ post.excerpt }}
-            <br />
-            <time>Date posted: {{ post.updated_at }}</time>
-            <br />
-            <nuxt-link :to="{ path: post.slug }">View this post!</nuxt-link>
           </div>
         </div>
       </div>
+      <div class="column"></div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
