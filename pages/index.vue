b870c405b8ce19ead610d2d6e7acb925037fc22c
Simon Measures <pastorsi@gmail.com>
2020-02-14 17:48:07 +0000

    Changed the layout of the home page so that text introducing us is now below the hero and therefore more easily visible.

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,18 @@
       <div class="hero-body">
         <div class="container has-text-left">
           <h1 class="title has-text-warning">{{ page.title }}</h1>
-          <h1 v-html="page.html" class="subtitle">
-            {{ page.html }}
-          </h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <div class="columns">
+          <div class="column"></div>
+          <div class="column is-two-thirds">
+            <h1 class="title has-text-info">{{ page.title }}</h1>
+            <div v-html="page.html">{{ page.html }}</div>
+          </div>
+          <div class="column"></div>
         </div>
       </div>
     </section>
@@ -44,8 +53,7 @@ export default {
   background-position: 50% 0%;
   background-size: cover;
 }
-.subtitle,
-.title {
+.subtitle {
   background-color: rgba(37, 54, 39, 0.3);
 }
 </style>
