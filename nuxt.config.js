19aa2e8eeadbabf741f9174133c2173b7ec89375
Simon Measures <pastorsi@gmail.com>
2020-11-09 21:14:02 +0000

    Revert "Added dynamic routes to sitemap by using the Nuxt crawler and generator.ts"
    This reverts commit 595ca398a559895734b309ba20d3eff167ca83f6.

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,10 @@ export default {
   sitemap: {
     hostname: 'https://www.mcea.org.uk/',
     gzip: true,
-    exclude: ['/admin/**', '/admin'],
+    exclude: [
+      '/admin/**',
+      '/admin'
+    ],
     defaults: {
       changefreq: 'daily',
       priority: 1,
