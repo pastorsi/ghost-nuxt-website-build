f6bc5eb57ede037991c22bba7857a3ab9b21fc8a
Simon Measures <pastorsi@gmail.com>
2020-12-03 21:05:23 +0000

    Implemented Andrey Gaisinskii method for generating sitemap.xml which will include dynamic routes

---
+++ modules/generator.js
@@ -0,0 +1,13 @@
+/* eslint-disable require-await */
+const generator = function() {
+  this.nuxt.hook('generate:done', async (context) => {
+    const routesToExclude = process.env.NUXT_ENV_EXCLUDE_ROUTES
+      ? process.env.NUXT_ENV_EXCLUDE_ROUTES.split(',')
+      : []
+    const allRoutes = Array.from(context.generatedRoutes)
+    const routes = allRoutes.filter((route) => !routesToExclude.includes(route))
+    this.nuxt.options.sitemap.routes = [...routes]
+  })
+}
+
+export default generator
